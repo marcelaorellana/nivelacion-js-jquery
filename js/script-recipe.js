fb7f84587382a71ea25e35c249fe8d969015501b
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-10 22:48:11 -0400

    si se hace click en botón Recipe se de agrega la clase active, y se le qita la misma clase al boton make it. Si se hace click en el botón Make it, entonces se le agrega la clase active y se le quita la misma clase al botón Recipe

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -3,8 +3,12 @@ $(document).ready( function(){
 	console.log("Página Cargada");
 	$(".js-show-make").click(function(){
 		$(".page").addClass("make");
+		$(this).addClass("active");
+		$(".js-show-recipe").removeClass("active");
 	});
 	$(".js-show-recipe").click(function(){
 		$(".page").removeClass("make");
+		$(this).addClass("active");
+		$(".js-show-make").removeClass("active");
 	});
 });
