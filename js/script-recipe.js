3eb0ad1788965ec8f1ba8df649c9e7b01e16b4c4
marcelaorellana <marcelapaz.1985@gmail.com>
2017-07-10 22:41:13 -0400

    se agregaron dos funciones al js de recipe, el botón de recipe quita la clase make y se agranda el encabezado, y botón make it agrega la clase make que vuelve a achicar el encabezado

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,4 +1,10 @@
 $(document).ready( function(){
 	$(".recipe .js-menu").hide();
 	console.log("Página Cargada");
+	$(".js-show-make").click(function(){
+		$(".page").addClass("make");
+	});
+	$(".js-show-recipe").click(function(){
+		$(".page").removeClass("make");
+	});
 });
